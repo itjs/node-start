fcbc72a0a2e6652f3ca9bb43f06162d2fb7678eb
itjs <simpsonbradi@spu.edu>
2019-05-04 15:05:49 -0700

    With Gender Column, Local Host

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ const server = http.createServer((request, response) => {
         '                                       <th scope="col">First Name</th> \n' +
         '                                       <th scope="col">Last Name</th> \n' +
         '                                       <th scope="col">Phone</th> \n' +
+        '                                       <th scope="col">Gender</th> \n' +
         '                               </tr> \n' +
         '                       </thead> \n' +
         '                       <tbody> \n'
@@ -47,7 +48,7 @@ const server = http.createServer((request, response) => {
 	                '                                       <td>' + friends[key][f]["firstName"] + '</td> \n' +
 	                '                                       <td>' + friends[key][f]["lastName"] + '</td> \n' +
 	                '                                       <td>' + friends[key][f]["phone"] + '</td> \n' +
-                  '                                       <td>' + friends[key][f]["gender"] + '</td> \n'
+                  '                                       <td>' + friends[key][f]["gender"] + '</td> \n' +
 	                '                               </tr> \n'
 	            );
 
